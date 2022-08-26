e3890ffbc2c3349a661dff17a0ccf8da825949a1
rafifi7 <79894190+rafifi7@users.noreply.github.com>
2022-08-26 12:36:50 -0400

    Add files via upload

---
+++ Project01/Rafee-Adnan-Project01.py
@@ -0,0 +1,6 @@
+my_list = [1, 2, 3]
+print(f'My list is: {my_list}')
+
+1000 * 128
+
+1000 * 256
---
+++ Project01/Rafee-Adnan-Project01.r
@@ -0,0 +1,10 @@
+system("hostname", intern=TRUE)
+
+1000 * 128
+
+1000 * 256
+
+dat <- read.csv("/anvil/projects/tdm/data/disney/flight_of_passage.csv")
+
+head(dat)
+
